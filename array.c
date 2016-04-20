3b35561345362d1a9c135baed0d7a5de6cb1d979
sternritter12 <ciscoinpg@gmail.com>
2016-04-20 08:46:03 +0200

    #2

---
+++ array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int** create(int row, int col)
+{
+   int **tab=(int**)calloc(x*sizeof(int*));
+   if(tab==NULL)
+   {
+	printf("Blad\n");
+	return tab;
+   }
+//niedokonczone
+}
